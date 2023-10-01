7841dc26406142bd3ed2b6d39f99dd55e06307e3
Iris Jeong <77691556+iris-jeong@users.noreply.github.com>
2023-10-01 16:53:10 -0700

    Fix useCallBack dependency array in CircleEditor

--- components/CircleEditor.js
+++ components/CircleEditor.js
@@ -23,25 +23,28 @@ export default function CircleEditor({
 	const editorRef = useRef(null);
 	const lockIconRef = useRef(null);
 
-	const handleOutsideClick = useCallback((event) => {
-		if (showEditor) {
-			//If the user clicked inside of the color palette, do nothing.
-			if (
-				sketchPickerRef.current?.contains(event.target) || //Clicked inside the sketch picker.
-				(editorRef.current?.contains(event.target) &&
-					!lockIconRef.current?.contains(event.target)) //Clicked on either copy or palette icon.
-			) {
-				return;
-			} else if (lockIconRef.current?.contains(event.target)) {
-				//Click inside lock icon.
-				setIsPaletteOpen(false);
-			} else {
-				// Close the color palette and editor.
-				setIsPaletteOpen(false);
-				setShowEditor(false);
+	const handleOutsideClick = useCallback(
+		(event) => {
+			if (showEditor) {
+				//If the user clicked inside of the color palette, do nothing.
+				if (
+					sketchPickerRef.current?.contains(event.target) || //Clicked inside the sketch picker.
+					(editorRef.current?.contains(event.target) &&
+						!lockIconRef.current?.contains(event.target)) //Clicked on either copy or palette icon.
+				) {
+					return;
+				} else if (lockIconRef.current?.contains(event.target)) {
+					//Click inside lock icon.
+					setIsPaletteOpen(false);
+				} else {
+					// Close the color palette and editor.
+					setIsPaletteOpen(false);
+					setShowEditor(false);
+				}
 			}
-		}
-	}, []);
+		},
+		[setIsPaletteOpen, setShowEditor, showEditor]
+	);
 
 	const displayNotification = (type) => {
 		setIsNotificationShown(true);
